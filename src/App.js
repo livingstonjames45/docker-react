174f1f4f17078cc8a01c8c4a04bc267649b7390f
Craig <Craig@DESKTOP-TS7LQGS>
2021-01-01 10:44:48 -0600

    slight change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          OMG SO MUCH KNOWLEDGE
+          Whee BeanStalk ftw
         </p>
         <a
           className="App-link"
